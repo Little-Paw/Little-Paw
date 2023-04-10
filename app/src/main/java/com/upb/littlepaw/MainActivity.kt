b6e26e78674c9b9d7c4116547dd084bc92847b6c
camigrandy <camgany@gmail.com>
2023-04-10 10:01:51 -0400

    Add Pet screen added.

--- app/src/main/java/com/upb/littlepaw/MainActivity.kt
+++ app/src/main/java/com/upb/littlepaw/MainActivity.kt
@@ -7,5 +7,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val addPetFragment = AddPetFragment()
+        supportFragmentManager.beginTransaction().add(R.id.fragment_container, addPetFragment).commit()
+
     }
 }
