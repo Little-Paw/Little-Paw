751fc6577584508c1aba382c7dd40ccc59616d88
ElJoamy <joamysalguero1@gmail.com>
2023-04-10 21:39:35 -0400

    login and register

--- app/src/main/java/com/upb/littlepaw/MainActivity.kt
+++ app/src/main/java/com/upb/littlepaw/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.upb.littlepaw
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val registerButton = findViewById<Button>(R.id.registerButton)
+        registerButton.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
